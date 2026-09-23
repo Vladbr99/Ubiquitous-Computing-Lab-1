9314a8d855d05b4e801dc5fddaacab988e6f1b1a
Vladbr99 <b00162071@mytudublin.ie>
2026-09-23 19:34:49 +0100

    Layout change, added 2 variables for game state, references for the EditText, TextViews etc,
    Changed to RelativeLayout since it was breaking my code with constraint.
    completed all that part and its time to start the logic part after this commit

--- Part2/app/src/main/java/com/example/lab1part2/MainActivity.java
+++ Part2/app/src/main/java/com/example/lab1part2/MainActivity.java
@@ -7,18 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int secretNumber;
+    private int guessCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        EditText editGuess = findViewById(R.id.editGuess);
+        TextView textFeedback = findViewById(R.id.textFeedback);
+        TextView textGuessCount = findViewById(R.id.textGuessCount);
+        Button buttonGuess = findViewById(R.id.buttonGuess);
+        Button buttonPlayAgain = findViewById(R.id.buttonPlayAgain);
     }
 }
