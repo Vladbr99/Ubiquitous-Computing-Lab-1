9e83b95de6b1ce2bc042466bf5f474d86a54d412
Vladbr99 <b00162071@mytudublin.ie>
2026-09-26 13:09:23 +0100

    Fixed a lot of problems, when starting i used a wrong template so ended up deleting all the useless files
    Moved evrything into MainActivity by pasting my code from another file that i used that worked. Validations now finally work because i didnt edit my ids in my xml, they finally have correct laebls and i used them in my main code so now they are implemented in the main where it all works and also the toast fianlly works when evertyhing is inputted correctly.
    
    ITS A BIG MESS, but this thought me how to use android studio now!!

--- Part1/app/src/main/java/com/example/lab1/MainActivity.java
+++ Part1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,85 +1,52 @@
 package com.example.lab1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.lab1.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        EditText name = findViewById(R.id.editName);
+        EditText password = findViewById(R.id.editPassword);
+        EditText phone = findViewById(R.id.editPhone);
+        EditText email = findViewById(R.id.editEmail);
+        Button submit = findViewById(R.id.buttonSubmit);
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        submit.setOnClickListener(v -> {
+
+            String n = name.getText().toString().trim();
+            String p = password.getText().toString().trim();
+            String ph = phone.getText().toString().trim();
+            String em = email.getText().toString().trim();
+
+            // Validate name (letters only)
+            if (!n.matches("[A-Za-z ]+")) {
+                name.setError("Letters only");
+                return;
+            }
+
+            // Validate phone (digits only)
+            if (!ph.matches("\\d+")) {
+                phone.setError("Digits only");
+                return;
+            }
+
+            // Validate email
+            if (!em.contains("@") || !em.contains(".")) {
+                email.setError("Invalid email");
+                return;
+            }
+
+            Toast.makeText(this,
+                    "Thank you " + n + ", your request is being processed",
+                    Toast.LENGTH_LONG).show();
         });
-
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment_content_main);
-
-        if (navHostFragment != null) {
-            NavController navController = navHostFragment.getNavController();
-
-            appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment_content_main);
-        boolean handled = false;
-        if (navHostFragment != null) {
-            NavController navController = navHostFragment.getNavController();
-            handled = NavigationUI.navigateUp(navController, appBarConfiguration);
-        }
-        return handled || super.onSupportNavigateUp();
     }
 }
